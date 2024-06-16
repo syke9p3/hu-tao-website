34b2688af33cb54419ff2f154bc2648cb413c6f7
syke9p3 <gargarkenth93@gmail.com>
2024-06-16 19:55:26 +0800

    chore: Update Container component padding in responsive view

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="container px-12 mx-auto max-w-7xl xl:px-6">{children}</div>
+    <div className="container px-4 sm:px-12 mx-auto max-w-7xl xl:px-6">{children}</div>
   );
 };
 
